9940909bee709e519933d728fe4107fa2c4da5e5
Kekuz <anton.edlenko@yandex.ru>
2022-05-31 22:23:08 +0300

    More activities, menu is working.

--- app/src/main/java/com/example/studycontrol/MainActivity.kt
+++ app/src/main/java/com/example/studycontrol/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.studycontrol
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun chatMe(view: View){
+        val chatIntent = Intent(this, ChatActivity::class.java)
+        startActivity(chatIntent)
+    }
+
+    fun settingsMe(view: View){
+        val settingsIntent = Intent(this, SettingsActivity::class.java)
+        startActivity(settingsIntent)
+    }
+    fun homeMe(view: View){
+        val homeIntent = Intent(this, MainActivity::class.java)
+        startActivity(homeIntent)
     }
 }
